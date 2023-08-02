875de60ecf912f359ec175ec1fa3061b9a8b9a94
kkanggu <rica742244@gmail.com>
2023-08-02 22:00:14 +0900

    test: 테스트 수정

--- common/src/test/kotlin/com/example/domain/course/service/CourseServiceTest.kt
+++ common/src/test/kotlin/com/example/domain/course/service/CourseServiceTest.kt
@@ -3,28 +3,22 @@ package com.example.domain.course.service
 import com.example.common.enums.RegisterStatus
 import com.example.domain.course.model.repository.UserCourseRelationRepository
 import com.example.domain.main.model.repository.UserRepository
-import jakarta.transaction.Transactional
+import com.example.utils.IntegrationTest
 import org.assertj.core.api.Assertions.*
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
-import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.test.context.ActiveProfiles
-import org.springframework.test.context.TestConstructor
 import org.springframework.test.context.jdbc.Sql
 
-@SpringBootTest
-@TestConstructor(autowireMode = TestConstructor.AutowireMode.ALL)
-@ActiveProfiles("local")
 @Sql("classpath:/test.sql")
-@Transactional
+@IntegrationTest
 class CourseServiceTest(
     private val courseService: CourseService,
     private val userRepository: UserRepository,
     private val userCourseRelationRepository: UserCourseRelationRepository
 ) {
     @Test
-    @DisplayName("성공 - user_id를 이용하여 과목을 가져온다")
+    @DisplayName("성공_정상적인 과목 조회 시_user_id를 이용하여 과목을 가져온다")
     fun success_get_course_with_user_id() {
         userRepository.findAll().forEach { user ->
             val courses = userCourseRelationRepository.findByUserId(userId = user.id).filter {
