b1960f13f9ad7196a453d227725597e86279636d
kkanggu <rica742244@gmail.com>
2023-08-03 20:53:08 +0900

    test: 테스트용 Annotation 추가

--- common/src/test/kotlin/com/example/utils/IntegrationTest.kt
+++ common/src/test/kotlin/com/example/utils/IntegrationTest.kt
@@ -2,11 +2,8 @@ package com.example.utils
 
 import jakarta.transaction.Transactional
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.test.context.ActiveProfiles
-import org.springframework.test.context.TestConstructor
 
 @SpringBootTest
-@TestConstructor(autowireMode = TestConstructor.AutowireMode.ALL)
-@ActiveProfiles("local")
 @Transactional
+@UnitTest
 annotation class IntegrationTest()
