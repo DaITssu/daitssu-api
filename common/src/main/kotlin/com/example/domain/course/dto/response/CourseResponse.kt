d9c478a11d22d12504cecda58b52178b9431878a
swdevsw98 <102571918+swdevsw98@users.noreply.github.com>
2023-08-06 13:44:54 +0900

    refactor : CourseResponse변경

--- common/src/main/kotlin/com/example/domain/course/dto/response/CourseResponse.kt
+++ common/src/main/kotlin/com/example/domain/course/dto/response/CourseResponse.kt
@@ -2,7 +2,7 @@ package com.example.domain.course.dto.response
 
 class CourseResponse (
     val name: String,
-    val videos: List<VideoResponse>? = emptyList(),
-    val assignments: List<AssignmentResponse>? = emptyList(),
+    val videos: List<VideoResponse> = emptyList(),
+    val assignments: List<AssignmentResponse> = emptyList(),
     val term: Int
 )
