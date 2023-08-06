15cd56582275220c2dc46e6b5056e6b95769b3d7
swdevsw98 <102571918+swdevsw98@users.noreply.github.com>
2023-08-06 11:13:13 +0900

    refactor : 리스트가 없을때 빈 List로 변경

--- common/src/main/kotlin/com/example/domain/course/dto/response/CourseResponse.kt
+++ common/src/main/kotlin/com/example/domain/course/dto/response/CourseResponse.kt
@@ -2,7 +2,7 @@ package com.example.domain.course.dto.response
 
 class CourseResponse (
     val name: String,
-    val videos: List<VideoResponse>? = null,
-    val assignments: List<AssignmentResponse>? = null,
+    val videos: List<VideoResponse>? = emptyList(),
+    val assignments: List<AssignmentResponse>? = emptyList(),
     val term: Int
 )
