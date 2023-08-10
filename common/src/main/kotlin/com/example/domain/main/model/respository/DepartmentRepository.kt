519477283a4c4c63d63ebcaadd68e6be79720dee
swdevsw98 <102571918+swdevsw98@users.noreply.github.com>
2023-08-10 17:35:54 +0900

    Conflict 수정

--- common/src/main/kotlin/com/example/domain/main/model/respository/DepartmentRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.example.domain.main.model.repository
-
-import com.example.domain.main.model.entity.Department
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface DepartmentRepository : JpaRepository<Department, Long> {
-    fun findByName(name: String): Department?
-}
