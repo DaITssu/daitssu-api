d3b1880f25da6def53fe06d8a4013f4e6cc8ca1c
kkanggu <rica742244@gmail.com>
2023-08-12 21:50:39 +0900

    feat: Entity 추가

---
+++ src/main/kotlin/com/example/daitssuapi/domain/main/model/entity/User.kt
@@ -0,0 +1,20 @@
+package com.example.daitssuapi.domain.main.model.entity
+
+import com.example.daitssuapi.common.audit.BaseEntity
+import jakarta.persistence.*
+
+@Entity
+@Table(schema = "main", name = "users")
+class User(
+    val studentId: Int,
+
+    val name: String,
+
+    val nickname: String? = null,
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "department_id")
+    val department: Department,
+
+    val imageUrl: String? = null
+) : BaseEntity()
