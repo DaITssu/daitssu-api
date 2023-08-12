57129d27d49c86b2afb23eef2880b67909191da8
CreatePath <hary0427@naver.com>
2023-08-13 03:10:44 +0900

    refactor: NO_NICKNAME -> NICKNAME_REQUIRED로 수정, ARTICLE_NOT_FOUND 에러 추가

---
+++ common/src/main/kotlin/com/example/common/enums/ErrorCode.kt
@@ -0,0 +1,16 @@
+package com.example.common.enums
+
+private const val MAIN_NUMBERING = 1000
+private const val NOTICE_NUMBERING = 2000
+private const val COURSE_NUMBERING = 3000
+private const val SERVER_NUMBERING = 4000
+
+enum class ErrorCode(val code: Int, val message: String) {
+    USER_NOT_FOUND(MAIN_NUMBERING + 1, "유저를 찾을 수 없습니다"),
+    DEPARTMENT_NOT_FOUND(MAIN_NUMBERING + 2, "학과를 찾을 수 없습니다"),
+    ARTICLE_NOT_FOUND(MAIN_NUMBERING + 3, "게시글을 찾을 수 없습니다."),
+    NICKNAME_REQUIRED(MAIN_NUMBERING + 4, "닉네임이 필요한 작업입니다."),
+
+    BAD_REQUEST(SERVER_NUMBERING + 1, "잘못된 요청입니다."),
+    INVALID_FORMAT(SERVER_NUMBERING + 2, "잘못된 형식입니다."),
+}
