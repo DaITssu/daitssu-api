3a15eda8a4aa9019ee4821548f3b189cbe64b195
CreatePath <hary0427@naver.com>
2023-08-13 03:09:02 +0900

    docs: swagger 작성

--- src/main/kotlin/com/example/daitssuapi/domain/main/dto/request/ArticleWritingRequest.kt
+++ src/main/kotlin/com/example/daitssuapi/domain/main/dto/request/ArticleWritingRequest.kt
@@ -1,11 +1,23 @@
 package com.example.daitssuapi.domain.main.dto.request
 
 import com.example.daitssuapi.domain.main.enums.Topic
+import io.swagger.v3.oas.annotations.media.Schema
 
+@Schema(description = "커뮤니티 게시글 작성 API request body")
 data class ArticleWritingRequest(
+    @Schema(
+        description = "게시글 주제",
+        allowableValues = ["CHAT", "INFORMATION", "QUESTION"]
+    )
     val topic: Topic,
+
+    @Schema(description = "게시글 제목")
     val title: String,
+
+    @Schema(description = "게시글 내용")
     val content: String,
+
+    @Schema(description = "작성자 닉네임")
     val nickname: String? = null
 //    val studentId: Int,
 //    val images, // image 데이터
--- src/main/kotlin/com/example/daitssuapi/domain/main/dto/response/ArticleResponse.kt
+++ src/main/kotlin/com/example/daitssuapi/domain/main/dto/response/ArticleResponse.kt
@@ -1,13 +1,30 @@
 package com.example.daitssuapi.domain.main.dto.response
 
+import io.swagger.v3.oas.annotations.media.Schema
 import java.time.LocalDateTime
 
+@Schema(description = "단일 게시글 정보")
 data class ArticleResponse(
+    @Schema(description = "게시글 id")
     val id: Long,
+
+    @Schema(
+        description = "게시글 주제",
+        allowableValues = ["잡담", "정보", "질문"]
+    )
     val topic: String,
+
+    @Schema(description = "게시글 제목")
     val title: String,
+
+    @Schema(description = "게시글 내용")
     val content: String,
+
+    @Schema(description = "작성자 닉네임")
     val writerNickName: String,
-//    val images // image 데이터
+
+    @Schema(description = "마지막 수정된 시각")
     val updatedAt: LocalDateTime,
+
+//    val images // image 데이터
 )
