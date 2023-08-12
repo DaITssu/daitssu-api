6bf21d2e23c0cdb6df928d3799e715dc9e785e77
CreatePath <hary0427@naver.com>
2023-08-13 03:09:02 +0900

    docs: swagger 작성

--- common/src/main/kotlin/com/example/domain/main/controller/ArticleController.kt
+++ common/src/main/kotlin/com/example/domain/main/controller/ArticleController.kt
@@ -4,19 +4,43 @@ import com.example.common.dto.Response
 import com.example.domain.main.dto.request.ArticleWritingRequest
 import com.example.domain.main.dto.response.ArticleResponse
 import com.example.domain.main.service.ArticleService
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.Parameter
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/daitssu/community/article")
+@Tag(name = "article", description = "커뮤니티 게시글 API")
 class ArticleController(
     private val articleService: ArticleService
 ) {
+    @Operation(
+        summary = "게시글 단일 조회",
+        responses = [
+            ApiResponse(
+                responseCode = "200",
+                description = "OK"
+            )
+        ]
+    )
     @GetMapping("/{articleId}")
     fun getArticle(
+        @Parameter(name = "articleId", description = "게시글 id")
         @PathVariable articleId: Long
     ): Response<ArticleResponse>
         = Response(data = articleService.getArticle(articleId))
 
+    @Operation(
+        summary = "새로운 게시글 작성",
+        responses = [
+            ApiResponse(
+                responseCode = "200",
+                description = "OK"
+            )
+        ]
+    )
     @PostMapping
     fun writeArticle(
         @RequestBody articleWritingRequest: ArticleWritingRequest
