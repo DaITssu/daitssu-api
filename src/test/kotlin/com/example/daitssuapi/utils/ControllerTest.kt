409dfbe3ac4ecb52b80a2a1f4052eca5aaa8fc75
kkanggu <rica742244@gmail.com>
2023-08-12 21:53:29 +0900

    test: 테스트용 Annotation 추가

---
+++ src/test/kotlin/com/example/daitssuapi/utils/ControllerTest.kt
@@ -0,0 +1,7 @@
+package com.example.daitssuapi.utils
+
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+
+@AutoConfigureMockMvc
+@IntegrationTest
+annotation class ControllerTest
---
+++ src/test/kotlin/com/example/daitssuapi/utils/IntegrationTest.kt
@@ -0,0 +1,9 @@
+package com.example.daitssuapi.utils
+
+import jakarta.transaction.Transactional
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+@Transactional
+@UnitTest
+annotation class IntegrationTest
