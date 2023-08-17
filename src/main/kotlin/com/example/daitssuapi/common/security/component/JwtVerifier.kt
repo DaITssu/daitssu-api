d4f4feeba8026f08668cccdf34d79158fffc3f46
kkanggu <rica742244@gmail.com>
2023-08-17 23:54:40 +0900

    chore: 개발, 로컬 환경에서 Token 검증하지 않도록 수정

--- src/main/kotlin/com/example/daitssuapi/common/security/component/JwtVerifier.kt
+++ src/main/kotlin/com/example/daitssuapi/common/security/component/JwtVerifier.kt
@@ -7,12 +7,16 @@ import com.auth0.jwt.exceptions.JWTVerificationException
 import com.auth0.jwt.exceptions.TokenExpiredException
 import com.example.daitssuapi.common.dto.TokenDto
 import jakarta.servlet.http.HttpServletRequest
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 
 @Component
 class JwtVerifier(
     private val tokenAlgorithm: Algorithm,
+    @Value("\${spring.profiles.active}") private val profile: String
 ) {
+    private val alwaysAllowProfiles = listOf("dev", "local")
+
     val tokenVerifier: JWTVerifier = JWT
         .require(tokenAlgorithm)
         .withClaimPresence("userId")
@@ -20,32 +24,27 @@ class JwtVerifier(
         .build()
 
     fun verify(request: HttpServletRequest): TokenDto? {
+        if (profile in alwaysAllowProfiles) {
+            return TokenDto(userId = 0, userRole = "STUDENT")
+        }
+
         val bearerToken = request.getHeader("Authorization") ?: return null
-        if (!bearerToken.startsWith("Bearer ")) return null
+        if (!bearerToken.startsWith("Bearer ")) {
+            return null
+        }
 
         return verifyToken(bearerToken)
     }
 
     fun verifyToken(bearerToken: String): TokenDto {
         try {
-            val verification = when (val token = bearerToken.substring(7)) {
-                // 개발
-                "test"
-                -> TokenDto(
-                    userId = 1,
-                    userRole = "STUDENT"
-                )
+            val token = bearerToken.substring(7)
+            val verifiedJWT = tokenVerifier.verify(token)
 
-                else -> {
-                    val verifiedJWT = tokenVerifier.verify(token)
-
-                    TokenDto(
-                        userId = verifiedJWT.getClaim("userId").asLong(),
-                        userRole = verifiedJWT.getClaim("userRole").asString(),
-                    )
-                }
-            }
-            return verification
+            return TokenDto(
+                userId = verifiedJWT.getClaim("userId").asLong(),
+                userRole = verifiedJWT.getClaim("userRole").asString(),
+            )
         } catch (e: TokenExpiredException) {
             throw RuntimeException("토큰이 만료되었습니다.") // JwtTokenExpiredException()
         } catch (e: JWTVerificationException) {
