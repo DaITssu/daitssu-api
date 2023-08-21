b27d31aae18395a67ec94e52d978a5034b79bd25
kkanggu <rica742244@gmail.com>
2023-08-21 23:28:17 +0900

    test: 테스트용 Annotation 추가

---
+++ src/test/kotlin/com/example/daitssuapi/utils/IntegrationTest.kt
@@ -0,0 +1,9 @@
+package com.example.daitssuapi.utils
+
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.transaction.annotation.Transactional
+
+@SpringBootTest
+@Transactional
+@UnitTest
+annotation class IntegrationTest
