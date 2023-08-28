501353708475a26e005b1ec48c187032449be560
JooHui-void <elis100@naver.com>
2023-08-28 21:52:33 +0900

    feat: 로컬 데이터베이스 생성 sql 추가

--- src/test/resources/schema.sql
+++ src/test/resources/schema.sql
@@ -1,3 +1,26 @@
-CREATE SCHEMA main;
-CREATE SCHEMA notice;
-CREATE SCHEMA course;
+
+CREATE SCHEMA IF NOT EXISTS notice;
+CREATE TABLE IF NOT EXISTS notice.notice(
+    id IDENTITY PRIMARY KEY,
+    title VARCHAR(1024) NOT NULL,
+    department_id integer NOT NULL,
+    content VARCHAR(2048) NOT NULL,
+    category VARCHAR(32) NOT NULL,
+    image_url VARCHAR(2048) NULL,
+    file_url VARCHAR(2048) NULL,
+    created_at TIMESTAMP NULL,
+    updated_at TIMESTAMP NULL
+);
+
+CREATE TABLE IF NOT EXISTS notice.notice_fs(
+    id IDENTITY PRIMARY KEY,
+    title VARCHAR(1024) NOT NULL,
+    department_id integer NOT NULL,
+    content VARCHAR(2048) NOT NULL,
+    category VARCHAR(32) NOT NULL,
+    image_url VARCHAR(2048) NULL,
+    file_url VARCHAR(2048) NULL,
+    created_at TIMESTAMP NULL,
+    updated_at TIMESTAMP NULL
+);
+
