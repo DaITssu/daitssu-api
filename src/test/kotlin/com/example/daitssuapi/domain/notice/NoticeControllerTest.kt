c2e8b486a1f3db2aa9d5cc1fa2799b5c2223c5f5
JooHui-void <elis100@naver.com>
2023-09-02 16:57:30 +0900

    fix : 필요없는 어노테이션 제거, assert로 테스팅 반영

--- src/test/kotlin/com/example/daitssuapi/domain/notice/NoticeControllerTest.kt
+++ src/test/kotlin/com/example/daitssuapi/domain/notice/NoticeControllerTest.kt
@@ -2,22 +2,25 @@ package com.example.daitssuapi.domain.notice
 
 import com.example.daitssuapi.domain.notice.model.repository.NoticeRepository
 import com.example.daitssuapi.domain.notice.service.NoticeService
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.mockito.Mockito.*
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.test.web.servlet.MockMvc
-import org.springframework.test.web.servlet.get
-import org.springframework.boot.test.mock.mockito.MockBean
-import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext
 import org.springframework.security.test.context.support.WithMockUser
 import org.springframework.test.context.jdbc.Sql
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.get
+import org.springframework.test.web.servlet.setup.DefaultMockMvcBuilder
+import org.springframework.test.web.servlet.setup.MockMvcBuilders
+import org.springframework.web.context.WebApplicationContext
+import org.springframework.web.filter.CharacterEncodingFilter
 
 
 @AutoConfigureMockMvc
-@MockBean(JpaMetamodelMappingContext::class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
 class NoticeControllerTest {
     @Autowired
@@ -28,6 +31,16 @@ class NoticeControllerTest {
 
     @Autowired
     private lateinit var noticeRepository: NoticeRepository
+
+    @Autowired
+    private lateinit var ctx : WebApplicationContext
+
+    @BeforeEach
+    fun setup() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(ctx)
+            .addFilters<DefaultMockMvcBuilder>(CharacterEncodingFilter("UTF-8", true))
+            .build()
+    }
 
     @Sql("classpath:schema.sql")
     @Sql("classpath:data.sql")
@@ -43,7 +56,6 @@ class NoticeControllerTest {
 
             }
             .andReturn()
-
         println(result.response.contentAsString)
     }
 
@@ -54,16 +66,24 @@ class NoticeControllerTest {
     @DisplayName("Notice 리스트 카테고리별 검색 확인")
     fun getSomeNoticeList() {
 
-        val result = mockMvc.get("/notice/학사")
+        val result = mockMvc.get("/notice/국제교류")
             .andExpect {
                 status { isOk() }
 
             }
             .andReturn()
 
-        println(result.response.contentAsString)
+        assertEquals(result.response.contentAsString,
+            "{\"code\":0,\"message\":\"\",\"data\":[{\"id\":4," +
+                    "\"title\":\"공지사항4\"," +
+                    "\"departmentId\":4," +
+                    "\"content\":\"4번 공지 내용입니다!!\"," +
+                    "\"category\":\"국제교류\"," +
+                    "\"imageUrl\":null," +
+                    "\"fileUrl\":null," +
+                    "\"createdAt\":\"0999-12-27T00:32:08\"," +
+                    "\"updatedAt\":\"0999-12-27T00:32:08\"}]}")
     }
-
 
     @Sql("classpath:schema.sql")
     @Sql("classpath:data.sql")
@@ -77,7 +97,16 @@ class NoticeControllerTest {
 
             }
             .andReturn()
-        println(result.response.contentAsString)
+        assertEquals(result.response.contentAsString,
+            "{\"code\":0,\"message\":\"\",\"data\":{\"id\":1," +
+                    "\"title\":\"공지사항1\"," +
+                    "\"departmentId\":1," +
+                    "\"content\":\"1번 공지 내용입니다!!\"," +
+                    "\"category\":\"학사\"," +
+                    "\"imageUrl\":null," +
+                    "\"fileUrl\":null," +
+                    "\"createdAt\":\"0999-12-27T00:32:08\"," +
+                    "\"updatedAt\":\"0999-12-27T00:32:08\"}}" )
     }
 
 }
