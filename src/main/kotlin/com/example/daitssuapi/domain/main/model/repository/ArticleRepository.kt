57e8c41d5b62a36024242d45a81f552b832942cd
swdevsw98 <102571918+swdevsw98@users.noreply.github.com>
2023-10-17 13:46:55 +0900

    feat : 내가 쓴 글 조회

--- src/main/kotlin/com/example/daitssuapi/domain/main/model/repository/ArticleRepository.kt
+++ src/main/kotlin/com/example/daitssuapi/domain/main/model/repository/ArticleRepository.kt
@@ -1,6 +1,7 @@
 package com.example.daitssuapi.domain.main.model.repository
 
 import com.example.daitssuapi.domain.main.model.entity.Article
+import com.example.daitssuapi.domain.main.model.entity.User
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
@@ -11,4 +12,6 @@ interface ArticleRepository : JpaRepository<Article, Long> {
         content: String,
         pageable: Pageable,
     ): Page<Article>
+    
+    fun findAllByWriter(writer: User) : List<Article>
 }
