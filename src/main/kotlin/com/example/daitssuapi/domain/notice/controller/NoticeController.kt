5d70bd485fad21e0be8c71ed8e4c2e34aa165872
HwanGonJang <myggona@gmail.com>
2023-12-31 21:59:45 +0900

    fix: 공지 조회 수정

--- src/main/kotlin/com/example/daitssuapi/domain/notice/controller/NoticeController.kt
+++ src/main/kotlin/com/example/daitssuapi/domain/notice/controller/NoticeController.kt
@@ -6,12 +6,11 @@ import com.example.daitssuapi.common.security.component.ArgumentResolver
 import com.example.daitssuapi.domain.article.dto.request.CommentWriteRequest
 import com.example.daitssuapi.domain.article.dto.response.CommentResponse
 import com.example.daitssuapi.domain.notice.dto.NoticePageResponse
-import com.example.daitssuapi.domain.notice.dto.NoticeResponse
+import com.example.daitssuapi.domain.notice.dto.PageNoticeResponse
 import com.example.daitssuapi.domain.notice.service.NoticeService
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.Parameter
 import io.swagger.v3.oas.annotations.responses.ApiResponse
-import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.data.web.PageableDefault
 import org.springframework.web.bind.annotation.*
@@ -42,7 +41,7 @@ sort: [\"createdAt\"]
         )
         @PageableDefault(page = 0, size = 10, sort = ["createdAt"]) pageable: Pageable,
         @RequestParam searchKeyword: String? = null
-    ): Response<Page<NoticeResponse>> {
+    ): Response<PageNoticeResponse> {
         return Response(data = noticeService.getAllNoticeList(searchKeyword, pageable))
     }
 
@@ -57,7 +56,7 @@ sort: [\"createdAt\"]
         @RequestParam category: NoticeCategory,
         @RequestParam searchKeyword: String? = null,
         @PageableDefault(page = 0, size = 10, sort = ["createdAt"]) pageable: Pageable, // TODO : 이거 swagger에서 조작 불가
-    ): Response<Page<NoticeResponse>> {
+    ): Response<PageNoticeResponse> {
         return Response(data = noticeService.getNoticeList(category, searchKeyword, pageable))
     }
 
