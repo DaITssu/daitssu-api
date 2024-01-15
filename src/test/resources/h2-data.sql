74bca01946bc1822d34f4e9a23005858dc32d9c9
kkanggu <rica742244@gmail.com>
2024-01-15 21:23:47 +0900

    test: 테스트 sql 세미콜론 빠진 것 수정

--- src/test/resources/h2-data.sql
+++ src/test/resources/h2-data.sql
@@ -91,6 +91,7 @@ INSERT INTO notice_fs(id, title, content, category,url, image_url, created_at, u
     (5,'공지사항5','5번 공지 내용입니다!!','EXPERIENTIAL_ACTIVITIES','http://google.com', '{"url": []}', '1000-01-01 00:00:00','1000-01-01 00:00:00',0);
 
 INSERT INTO course_notice(id, course_id, is_active, name, registered_at, views, content, file_url) VALUES
-    (1, 1, true, '강의의 공지1', '2024-01-01 04:16:12', 3, '공지 상세 내용', '{"url": []}')
+    (1, 1, true, '강의의 공지1', '2024-01-01 04:16:12', 3, '공지 상세 내용', '{"url": []}');
+
 INSERT INTO service_notice(id, title, content, created_at, updated_at ) VALUES
     (1, '6/23 (금) 시스템 점검 및 업데이트 안내', '안녕하세요 다잇슈입니다. 더욱 쾌적하고 안정적인 서비스 지원을 위해 아래와 같이 점검이 진행됩니다.', '2023-12-06 06:44:07', '2023-12-06 06:44:07');
